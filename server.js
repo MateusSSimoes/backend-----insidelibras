60d72a06dbce3c5b124449df03d3375bbc75e332
Mateus Simoes Silva <mateusgamer626@gmail.com>
2025-06-19 16:16:15 -0300

    atualiza segurança geral

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.post('/send-email', async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER || 'InsideLibras.Form@gmail.com',
-      pass: process.env.EMAIL_PASS || 'ucjnswwgjmsqpqto',
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
